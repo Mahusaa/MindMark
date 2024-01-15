818192e1373347b72bcc89fb3c938dd3caf68afa
root <usamah.hafizh@ui.ac.id>
2024-01-15 15:17:05 +0700

    Set up client react-ts & tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div> 
+      <header>
+        <h1 className="text-3xl font-bold text-blue-700"> Hello World Makan </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
